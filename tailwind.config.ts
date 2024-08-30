4a94a8e6d619acae4c1618a72821b445acfae5b5
kumarnitish075 <nitishkumar868308@gmail.com>
2024-08-30 16:51:50 +0530

    ch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        "fade-in-out": {
+          "0%": { opacity: "0" },
+          "50%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "fade-in-out": "fade-in-out 5s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
